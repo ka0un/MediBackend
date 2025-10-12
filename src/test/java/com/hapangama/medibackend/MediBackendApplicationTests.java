259b1e94afe9e0a239ea08b0829046fc8012a31e
KASUNHapangama <88395585+KASUNHapangama@users.noreply.github.com>
2025-10-12 19:19:47 +0530

    Initial commit

---
+++ src/test/java/com/hapangama/medibackend/MediBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hapangama.medibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
