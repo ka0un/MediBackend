92bfb626b232107becab59786668e28ac886a539
ka0un <kasun@hapangama.com>
2025-10-26 01:49:15 +0530

    appointment sms improvements

--- src/main/java/com/hapangama/medibackend/service/NotificationServiceImpl.java
+++ src/main/java/com/hapangama/medibackend/service/NotificationServiceImpl.java
@@ -4,6 +4,8 @@ import com.hapangama.medibackend.model.Appointment;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.format.DateTimeFormatter;
+
 @RequiredArgsConstructor
 @Service
 public class NotificationServiceImpl implements NotificationService{
@@ -18,9 +20,13 @@ public class NotificationServiceImpl implements NotificationService{
     @Override
     public void sendSmsAppointmentConfirmation(Appointment appointment) {
         smsGateway.sendSms(appointment.getPatient().getPhone(),
-                "Dear " + appointment.getPatient().getName() +
-                        ", your appointment is confirmed for " + appointment.getBookingDateTime().toString() +
-                        ". Thank you!");
+                "Dear " + appointment.getPatient().getName() + ",\n\n" +
+                "Your appointment has been confirmed.\n" +
+                "Date & Time: " + DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm").format(appointment.getBookingDateTime()) + "\n" +
+                "Location: " + appointment.getProvider().getHospitalName() + "\n" +
+                "Appointment Code: " + appointment.getConfirmationNumber() + "\n\n" +
+                "Thank you,\n" +
+                appointment.getProvider().getHospitalName() + ".");
 
     }
 }
